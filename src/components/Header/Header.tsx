2f780b782ec09fa57341c6637503f87fb43f2420
davit <davidgyulinyan@gmail.com>
2024-04-08 11:43:33 +0400

    styles changed to tailwind

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,15 +1,14 @@
 import nasaLogo from '../../assets/nasa_logo.svg'
 import Lang from "../Lang/Lang"
-import './Header.css'
 
 const Header = () => {
     return (
         <header>
-            <div className="top-head flex-center">
-                <img src={nasaLogo} alt="NASA-logo"/>
-                <div>NASA Browser</div>
-            </div>
             <Lang/>
+            <div className="top-head w-full flex justify-center">
+                <img className="w-20 h-20" src={nasaLogo} alt="NASA-logo"/>
+                <div className="text-3xl font-bold flex justify-center">NASA Browser</div>
+            </div>
         </header>
     )
 }
--- src/components/Lang/Lang.tsx
+++ src/components/Lang/Lang.tsx
@@ -1,11 +1,27 @@
-import './Lang.css'
-
 const Lang = () => {
-    const langs = ['En', 'Ru', 'Am'];
+    const langs = [
+        {
+            id: 1,
+            title: "Am",
+            flag: ""
+        },
+
+        {
+            id: 2,
+            title: "En",
+            flag: ""
+        },
+
+        {
+            id: 3,
+            title: "Ru",
+            flag: ""
+        }
+    ];
     return (
-        <div className='langs flex-justify-end'>
+        <div className='langs w-[95%] flex justify-end items-end gap-3'>
             {
-                langs.map(lang => <a href='' key={lang}>{lang}</a>)
+                langs.map(lang => <span key={lang.id}>{lang.title}</span>)
             }
         </div>
     );
